1158be1612b99699438ded555d46cf33ec57c348
Filipe Alexandre Groh <filipegroh@gmail.com>
2021-05-28 12:15:41 -0300

    Initial

---
+++ game_FalconBms.py
@@ -0,0 +1,13 @@
+from ..basic_game import BasicGame
+
+
+class FalconBms(BasicGame):
+
+    Name = "Falcon BMS Plugin"
+    Author = "Cass"
+    Version = "0.0.1"
+
+    GameName = "Falcon BMS"
+    GameShortName = "falconBms"
+    GameBinary = r"Launcher.exe"
+    GameDataPath = r""
